420c27e2e032aae92a38bb78c8c3f89b95142c21
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-05 10:52:16 +0800

    Style NextJS project: use CSS modules

--- components/main-header.js
+++ components/main-header.js
@@ -1,18 +1,18 @@
 import Link from "next/link";
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 
 export default function MainHeader() {
-  return <header>
-    <Link href="/">
+  return <header className={classes.header}>
+    <Link className={classes.logo} href="/">
       <img src={logoImg.src} alt="A plate with food on it" />
+      NextLevel Food
     </Link>
 
-    <nav>
+    <nav className={classes.nav}>
       <ul>
         <li><Link href="/meals">Browse Meals</Link></li>
-      </ul>
-      <ul>
-      <li><Link href="/community">Foodies Community</Link></li>
+        <li><Link href="/community">Foodies Community</Link></li>
       </ul>
     </nav>
   </header>
