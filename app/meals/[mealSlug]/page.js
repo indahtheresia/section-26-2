07d1523ee76ca034605182aabf5c0a00bfbb2da5
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-06 10:27:10 +0800

    Throw Not Found errors for individual meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
 import { getMeal } from '@/lib/meals';
 import Image from 'next/image';
 import classes from './page.module.css';
+import { notFound } from 'next/navigation';
 
 export default function MealDetails({ params }) {
   const meal = getMeal(params.mealSlug);
+  
+  if (!meal) {
+    notFound();
+  }
   
   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
 
