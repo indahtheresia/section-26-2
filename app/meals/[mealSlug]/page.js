bd713d21ff13ae65a777f3bb7829a3a72d86947e
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-06 17:59:07 +0800

    Add dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,13 +3,24 @@ import Image from 'next/image';
 import classes from './page.module.css';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetails({ params }) {
   const meal = getMeal(params.mealSlug);
   
   if (!meal) {
     notFound();
   }
-  
+
   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
 
   return (<>
